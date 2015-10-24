874f3bc69dc81596695e5818a4ad46451f53949d
Whitney Murray <wnksmith92@yahoo.com>
2015-10-24 11:05:26 -0500

    began first part of quiz

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+var prompt=require('prompt-sync').prompt;
+
+console.log('Why hello there broteindo I hope you\'re fairing nicely today');
+console.log('If you want respect from anybody ever then you must complete this quiz');
+console.log('Now that we know eachother better what\' s your name\?');
+
+var playerName = prompt();
+
+console.log('Alright Dingus it is\! I am now going to throw my quiz at you I hope you have prepared yourself');
+
+
