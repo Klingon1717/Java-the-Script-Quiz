c4409812687e2be1f4b5a26fb736694173f7826d
Whitney Murray <wnksmith92@yahoo.com>
2015-11-22 20:06:22 -0600

    fixed percent at end of test

--- scripts/main.js
+++ scripts/main.js
@@ -6,17 +6,15 @@ var correctAnswers =0;
 var wrongAnswers =0;
 
 console.log('Why hello there broteindo I hope you\'re fairing nicely today');
-console.log('If you want respect from anybody ever then you must complete this quiz');
-console.log('Now that we know eachother better what\' s your name\?');
-
+console.log('What shall I call you');
 var playerName = prompt();
+console.log(playerName + ' ' + 'if you want respect from anybody ever then you must complete this quiz');
 
-console.log('Alright Dingus it is\! I am now going to throw my quiz at you I hope you have prepared yourself');
 
 
-console.log('Question One: When using css what is the symbol for id\?  .  \# \{ \(');
-var userAns = prompt();
-if(userAns.toLowerCase === '\#'){
+console.log('Question One: When using css what is the symbol for id\? \.\# \{ \(');
+var userAnsOne = prompt();
+if(userAnsOne === "#"){
 	correctAnswers ++;
 	console.log('you got it right you prefect beautiful specimen you\!');
 }
@@ -26,8 +24,8 @@ else{
 }
 
 console.log('Question Two: Can you add links in html\? yes or no');
-var userAns = prompt();
-if(userAns.toLowerCase ==='yes'){
+var userAnsTwo = prompt();
+if(userAnsTwo === "yes"){
 	correctAnswers++;
 	console.log('You got it right\! You are what I would consider perfection\!');
 }
@@ -37,8 +35,8 @@ else{
 }
 
 console.log('Question Three: Can you add images in css\? yes or no');
-var userAns = prompt();
-if(userAns.toLowerCase ==='yes'){
+var userAnsThree = prompt();
+if(userAnsThree === "yes"){
 	correctAnswers ++;
 	console.log('Can I have some your genius please\? You answered correctly\!');
 }
@@ -48,8 +46,8 @@ else{
 }
 
 console.log('Question Four:What is the correct symbol for calling a class in css\? \{ \# \. \[');
-var userAns = prompt();
-if(userAns.toLowerCase ==='.'){
+var userAnsFour = prompt();
+if(userAnsFour ==="."){
 	correctAnswers ++;
 	console.log('Correct\! I have to say I would not mind stealing your brain\!');
 }
@@ -59,8 +57,8 @@ else{
 }
 
 console.log('Question Five: Do you have to add a class and id to everything in a html document\? yes or no');
-var userAns = prompt();
-if(userAns.toLowerCase ==='no'){
+var userAnsFive = prompt();
+if(userAnsFive ==="no"){
 	correctAnswers ++;
 	console.log('I have never seen anyone ever get a question answer as right as you just did\!');
 }
@@ -70,8 +68,8 @@ else{
 }
 
 console.log('Question Six: If I put text in the title tag of a html doc will it be visible\? no or yes\?');
-var userAns = prompt();
-if(userAns.toLowerCase ==='no'){
+var userAnsSix = prompt();
+if(userAnsSix=== "no"){
 	correctAnswers ++;
 	console.log('Okay who am I dealing with here Einstein\? You are right\!');
 }
@@ -80,8 +78,8 @@ else{
 	wrongAnswers ++;
 }
 console.log('Question Seven: Is h1 font size bigger than h6\? yes or no');
-var userAns = prompt();
-if(userAns.toLowerCase ==='yes'){
+var userAnsSeven = prompt();
+if(userAnsSeven ==="yes"){
 	correctAnswers ++;
 	console.log('You have never been more beautiful than in this moment for your answer was right\!');
 }
@@ -91,8 +89,8 @@ else{
 }
 
 console.log('Question Eight: If elements were being displayed as block would they stack on top of each other\? yes or no');
-var userAns = prompt();
-if(userAns.toLowerCase === 'yes'){
+var userAnsEight = prompt();
+if(userAnsEight === "yes"){
 	correctAnswers ++;
 	console.log('Someone must have slipped their smart pants on because that answer was correct\!');
 }
@@ -102,8 +100,8 @@ else{
 }
 
 console.log('Question Nine:Can you link your css page under the body tag in your html doc\? yes or no');
-var userAns = prompt();
-if (userAns.toLowerCase === 'yes'){
+var userAnsNine = prompt();
+if (userAnsNine === "no"){
 	correctAnswers ++;
 	console.log('You are correct you can\! I mean it won\'t work but you can most certainly do it');
 }
@@ -113,8 +111,8 @@ else{
 }
 
 console.log('Question Ten:Which of the following is a html tag\? \<anchor\> \<p\> \<picture\> \<changecolor\>');
-var userAns = prompt();
-if(userAns.toLowerCase === '\<p\>'){
+var userAnsTen = prompt();
+if(userAnsTen === "<p>"){
 	correctAnswers ++;
 	console.log('The p should stand for perfect answer because that is what you gave');
 }
@@ -123,8 +121,8 @@ else{
 	wrongAnswers ++;
 }
 console.log('Question Eleven:Does css stand for complicated shiny shoes\? yes or no');
-var userAns = prompt();
-if(userAns.toLowerCase ==='yes'){
+var userAnsElv = prompt();
+if(userAnsElv === "yes"){
 	console.log('No you are not right in the least\!');
 	wrongAnswers ++;
 }
@@ -133,8 +131,8 @@ else{
 	correctAnswers ++;
 }
 console.log('Question Twelve: Would a \<a\> tag naturally dispaly block\? yes or no');
-var userAns = prompt();
-if(userAns.toLowerCase ==='no'){
+var userAnsTwe = prompt();
+if(userAnsTwe === "no"){
 	correctAnswers ++;
 	console.log('You did it! You answered the question correctly and I believe that you will never be more attractive than in this moment\!');
 }
@@ -143,8 +141,8 @@ else{
 	wrongAnswers ++;
 }
 console.log('Question Thirteen: Would a id override a class\? yes or no');
-var userAns = prompt();
-if(userAns.toLowerCase === 'yes'){
+var userAnsThi = prompt();
+if(userAnsThi === "yes"){
 	correctAnswers ++;
 	console.log('This will probably be the proudest moment of your life so don\'t forget it\! You answered the question correctly\!');
 	wrongAnswers ++;
@@ -153,8 +151,8 @@ else{
 	console.log('I do believe that I have yet to experience a question answered as wrong as you just answered this question\!');
 }
 console.log('Question Fourteen:If you were to write a paragraph in your html doc wrapped in \<div\> tags would the text be visible\? yes or no');
-var userAns = prompt();
-if(userAns.toLowerCase ==='yes'){
+var userAnsFourt = prompt();
+if(userAnsFourt === "yes"){
 	correctAnswers ++;
 	console.log('Your answer was not only incredibly impressive\! It was correct\!');
 }
@@ -163,8 +161,8 @@ else{
 	wrongAnswers ++;
 }
 console.log('Question Fifteen:Is it true that Steve Jobs created html css and the computer itself\? yes or no');
-var userAns = prompt();
-if(userAns.toLowerCase ==='yes'){
+var userAnsFif = prompt();
+if(userAnsFif === "yes"){
 	wrongAnswers ++;
 	console.log('I think someone may have a bit too much padding in the ol\' think box and not enough muscle\! For that was completely and utterly not the correct answer\!');
 }
@@ -174,8 +172,8 @@ else{
 }
 
 
-var percentTotal = (wrongAnswers+correctAnswers/15)*100;
-	console.log('percentTotal\%');
+var percentTotal = (correctAnswers /15)*100;
+	console.log(percentTotal +'\%');
 
 
 
